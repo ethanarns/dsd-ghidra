d66d8ffaf584e8deb07e43465c52116a84e523b3
Aetias <aetias@outlook.com>
2025-02-18 21:24:12 +0100

    Clean up imports

--- dsd-ghidra/src/main/java/dsdghidra/sync/DsdSyncFunction.java
+++ dsd-ghidra/src/main/java/dsdghidra/sync/DsdSyncFunction.java
@@ -5,17 +5,6 @@ import com.sun.jna.Structure;
 import dsdghidra.types.UnsafeList;
 import dsdghidra.types.UnsafeString;
 import dsdghidra.types.UnsafeU32List;
-import ghidra.app.cmd.function.CreateFunctionCmd;
-import ghidra.program.model.address.Address;
-import ghidra.program.model.address.AddressSet;
-import ghidra.program.model.listing.CircularDependencyException;
-import ghidra.program.model.listing.Function;
-import ghidra.program.model.listing.Listing;
-import ghidra.program.model.listing.Program;
-import ghidra.program.model.symbol.SourceType;
-import ghidra.util.exception.DuplicateNameException;
-import ghidra.util.exception.InvalidInputException;
-import ghidra.util.task.TaskMonitor;
 
 import java.util.List;
 
